1f1ff61696dbc0fb02043d38cbbc70de9d289be0
PacificD <pacificd148@gmail.com>
2024-03-08 11:44:35 +0800

    feat(*): content collections

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,30 @@
 import { defineConfig } from "astro/config";
 import react from "@astrojs/react";
 import mdx from "@astrojs/mdx";
-
+import AutoImport from "astro-auto-import";
 import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [react(), mdx(), tailwind()]
+  integrations: [
+    AutoImport({
+      imports: [
+        /**
+         * Generates:
+         * import Intro from './src/components/Intro.astro';
+         */
+        "./src/components/Intro.astro",
+        "./src/components/Note.astro",
+        /**
+         * Generates:
+         * import { Tweet, YouTube } from 'astro-embed';
+         */
+        { "astro-embed": ["YouTube"] },
+      ],
+    }),
+    react(),
+    mdx(),
+    tailwind(),
+  ],
 });
+
